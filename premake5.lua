3ed18c666f9053a228919637b8fe8f2791ce198e
Sri Sham Haran <s.srishamharan@digipen.edu>
2022-02-09 00:12:27 +0800

    Update for 1.85

---
+++ premake5.lua
@@ -0,0 +1,71 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_opengl3_loader.h",
+		"misc/cpp/**.h",
+		"misc/cpp/**.cpp",
+        "%{IncludeDir.imguizmo}/**.h",
+        "%{IncludeDir.imguizmo}/**.cpp",
+	}
+
+	includedirs
+	{
+	  "%{prj.location}",
+	  "%{IncludeDir.GLFW}",
+	  "%{IncludeDir.GLAD}"
+	}
+
+    excludes
+    {
+         "%{IncludeDir.imguizmo}/example**.h",
+         "%{IncludeDir.imguizmo}/example**.cpp",
+         "%{IncludeDir.imguizmo}/vcpkg-example**.h",
+         "%{IncludeDir.imguizmo}/vcpkg-example**.cpp",
+    }
+
+	links
+	{
+		"GLFW",
+		"GLAD",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
